786b5d5798a45aafc373a0e31ee4b967da3c4d5f
Arthur Abreu <Arthur Abreu>
2025-11-26 23:55:11 -0300

    Analisa o Idioma

---
+++ OneDrive/Imagens/Documentos/NetBeansProjects/treinos/src/main/java/com/mycompany/projetos/IdiomaDoSistema.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetos;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author arthu
+ */
+public class IdiomaDoSistema {
+    public static void main(String[] args) {
+         Locale idioma = Locale.getDefault();
+         System.out.println("A maquina está no idioma: " + idioma);
+    }
+}
